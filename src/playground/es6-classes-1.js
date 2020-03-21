ae7791978d9aa49babbd658afb498a7e336f529a
Fabian Seibt <faseibt@gmail.com>
2020-03-21 12:57:22 +0100

    Classes learned

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -4,7 +4,7 @@ class Person {
         this.name = name
         this.age = age
     }
-    getGretting() {
+    getGreeting() {
         return `Hi. I am ${this.name}!`
     }
     getDescription() {
@@ -12,8 +12,46 @@ class Person {
     }
 }
 
-const me = new Person("Fabian Seibt", 28)
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major
+    }
+    hasMajor() {
+        return !!this.major
+    }
+    getDescription() {
+        let description = super.getDescription()
+
+        if (this.hasMajor()) {
+            description += ` Their Major is ${this.major}`
+        }
+        return description
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age)
+        this.homeLocation = homeLocation
+    }
+    hasHomeLocation() {
+        return !!this.homeLocation
+    }
+    getGreeting() {
+        let greeting = super.getDescription()
+        if (this.hasHomeLocation()) {
+            greeting += ` I am visiting from ${this.homeLocation}`
+        }
+        return greeting
+    }
+}
+
+const me = new Traveler("Fabian", 28, "Spain")
+console.log(me.getGreeting())
 console.log(me.getDescription())
 
-const other = new Person()
+
+const other = new Traveler()
+console.log(other.getGreeting())
 console.log(other.getDescription())
